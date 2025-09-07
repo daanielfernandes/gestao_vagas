6387c997609c09792b6e7b98adc2bdcc48b69b7e
Daniel Fernandes <danielarruda99@gmail.com>
2025-09-07 00:16:55 -0300

    hash senha candidato

--- src/main/java/br/com/daniel/fernandes/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/daniel/fernandes/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.daniel.fernandes.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.daniel.fernandes.gestao_vagas.exceptions.UserFoundException;
@@ -13,6 +14,8 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent(
@@ -20,6 +23,11 @@ public class CreateCandidateUseCase {
                 throw new UserFoundException();
             }
         );
+        
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+
+        candidateEntity.setPassword(password);
+        
         return this.candidateRepository.save(candidateEntity);   
     }
 }
