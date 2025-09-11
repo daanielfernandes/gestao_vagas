dadee9aa23e993e1db747a7f2f139f0a07aca7fd
Daniel Fernandes <danielarruda99@gmail.com>
2025-09-10 23:46:12 -0300

    adicionando swagger no controller

--- src/main/java/br/com/daniel/fernandes/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/daniel/fernandes/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -16,6 +16,13 @@ import br.com.daniel.fernandes.gestao_vagas.modules.candidate.useCases.CreateCan
 import br.com.daniel.fernandes.gestao_vagas.modules.candidate.useCases.ListAllJobsUseCase;
 import br.com.daniel.fernandes.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import br.com.daniel.fernandes.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -62,6 +69,15 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas, baseada no filtro. ")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+            )
+        })
+    })
     public List<JobEntity> findJobByFilter(@RequestParam String filter) {
         return this.listAllJobsUseCase.execute(filter);
     }
